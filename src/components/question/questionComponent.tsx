90aa41a8c565eaeb58d3511a1bacc703cddfe1ec
DnyaneshD <dnyan.desh@gmail.com>
2019-05-28 23:55:47 +0200

    Update css

--- src/components/question/questionComponent.tsx
+++ src/components/question/questionComponent.tsx
@@ -13,7 +13,9 @@ export const Question = (props: IQuestion) => {
   return (
     <div>
       <div className="box">
-        <span className="text">{props.questionText}</span>
+        <span className="text">
+          <b>{props.questionText}</b>
+        </span>
         <span className="text">{dateFormatter(props.publishedAt)}</span>
         <span className="text">{props.lengthOfChoices}</span>
       </div>
